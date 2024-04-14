168efe41eb8d5387d7373c9c84f331184c5d9af4
Jörg <jpolkehn@gmail.com>
2024-04-14 22:36:59 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./Components/Header";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
