45de937d64207e0924328912f7f9a8bbd80dbffa
Jörg <jpolkehn@gmail.com>
2024-04-14 22:45:02 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["poppins", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+
+      colors: {
+        "fitness-color-dark": "#202430",
+        "fitness-color-medium": "#3A4151",
+        "fitness-color-light": "#202430",
+        "fitness-color-grandis": "#FFCC6A",
+        "fitness-color-light-pink": "#FE9EB9",
+        "fitness-color-turkis": "#4CF4E2",
+        "fitness-color-mint": "#EDFDA8",
+        "fitness-color-blue": "#3B5FE5",
+      },
+    },
   },
   plugins: [],
 };
