78020ccd6792c0c32e7b3e16543624f560c052d6
Jörg <jpolkehn@gmail.com>
2024-04-15 15:59:08 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,45 @@ module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["poppins", "sans-serif"],
-        serif: ["Merriweather", "serif"],
-      },
-
-      colors: {
+            colors: {
         "fitness-color-dark": "#202430",
         "fitness-color-medium": "#3A4151",
-        "fitness-color-light": "#202430",
-        "fitness-color-grandis": "#FFCC6A",
-        "fitness-color-light-pink": "#FE9EB9",
-        "fitness-color-turkis": "#4CF4E2",
-        "fitness-color-mint": "#EDFDA8",
-        "fitness-color-blue": "#3B5FE5",
+        "fitness-color-light": "#FBFBFB",
+        "fitness-color-yellow": "#FFD162",
+        "fitness-color-light-pink": "#FF99C3",
+        "fitness-color-light-green": "#F5FFA0",
+        "fitness-color-turkis": "#3EF3E8",
+        "fitness-color-mint": "#3EE4E8",
+        "fitness-color-blue": "#3B85E6",
+        "fitness-color-dark-blue": "#3A4AE4",
+      },
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+      fontSize: {
+        "size-xxl": "36px",
+        "size-xl": "24px",
+        "size-l": "18px",
+        "size-m": "16px",
+        "size-s": "12px",
+      },
+      fontWeight: {
+        bold: "700",
+        reg: "400",
+      },
+      lineHight: {
+        "line-xxl": "54px",
+        "line-xl": "36px",
+        "line-l": "27px",
+        "line-m": "21px",
+        "line-s": "15px",
+      },
+      typography: {
+        h1: {
+          fontSize: "size-xxl",
+          fontWeight: "bold",
+          lineHight: "line-xxl",
+        },
       },
     },
   },
